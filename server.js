a830bd2c16f05ab6debf2d5b713271a6c7f0ee18
Josh Hepworth <jrhepworth12@gmail.com>
2018-10-14 20:25:50 -0500

    app is listening, homepage set up

--- server.js
+++ server.js
@@ -2,3 +2,39 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const PORT = 3000;
+
+require('./db/db')
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'))
+
+app.get('/', (req, res) => {
+	res.send('Welcome to homepage!')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+	console.log('app listening on port ' + PORT)
+})
