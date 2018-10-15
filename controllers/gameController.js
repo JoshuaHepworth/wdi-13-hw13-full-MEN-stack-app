67fcc93ec033afb9ae6d40205342e80290f76773
Josh Hepworth <jrhepworth12@gmail.com>
2018-10-14 20:39:55 -0500

    added controllers and required modules, setup restful route format

---
+++ controllers/gameController.js
@@ -0,0 +1,18 @@
+const Game = require('../models/games.js');
+const express = require('express');
+const router = express.Router();
+
+
+//index
+
+//new
+
+//create
+
+//show
+
+//edit
+
+//uodate
+
+//destroy
