63755fff64756ba3c26c3ab35d0c0389256f930e
Josh Hepworth <jrhepworth12@gmail.com>
2018-10-15 09:12:43 -0500

    exported router, and new page works now for adding new items

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,15 +1,19 @@
 const Game = require('../models/games.js');
 const express = require('express');
 const router = express.Router();
-
+const mongoose = require('mongoose')
 
 //index
 router.get('/', (req, res) => {
 	Game.find({}, (err, foundGames) => {
-		res.render('index.ejs', {
+		if(err){
+			console.log(err)
+		} else {
+			res.render('index.ejs', {
 			foundGames: foundGames
-		});
-	});
+			})
+		}
+	})
 });
 //new
 router.get('/new', (req, res) => {
@@ -24,3 +28,8 @@ router.get('/new', (req, res) => {
 //uodate
 
 //destroy
+
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,14 +3,17 @@ const app = express();
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const PORT = 3000;
+const gameController = require('./controllers/gameController')
 
 require('./db/db')
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'))
 
+app.use('/games', gameController)
+
 app.get('/', (req, res) => {
-	res.send('Welcome to homepage!')
+	res.send('Welcome to homepage! <a href="/games/new">Add Game</a>')
 })
 
 
