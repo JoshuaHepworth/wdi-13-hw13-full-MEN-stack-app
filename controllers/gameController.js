ae95bbba0bbe939d366084f294549651ecb8c110
Josh Hepworth <jrhepworth12@gmail.com>
2018-10-15 20:28:47 -0500

    added routes

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,19 +1,14 @@
 const Game = require('../models/games.js');
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose')
 
 //index
 router.get('/', (req, res) => {
 	Game.find({}, (err, foundGames) => {
-		if(err){
-			console.log(err)
-		} else {
-			res.render('index.ejs', {
-			foundGames: foundGames
-			})
-		}
-	})
+		res.render('index.ejs', {
+			games: foundGames
+		});
+	});
 });
 //new
 router.get('/new', (req, res) => {
@@ -26,13 +21,45 @@ router.post('/', (req, res) => {
 	})
 })
 //show
-
+router.get('/:id', (req, res) => {
+	Game.findById(req.params.id, (err, foundGame) => {
+		res.render('games/show.ejs', {
+			game: foundGame
+		});
+	});
+});
 //edit
-
+router.get('/:id/edit', (req, res) => {
+	Game.findById(req.params.id, (err, editGame) => {
+		res.render('games/edit.ejs', {
+			game: editGame
+		});
+	});
+});
+//post
+router.post('/', (req, res) => {
+	console.log(req.body);
+	Game.create(req.body, (err, newGame) => {
+		if(err){
+			console.log(err);
+		}else{
+			res.redirect('/games')
+			console.log(newGame);
+		}
+	});
+});
 //update
-
+router.put('/:id', (req, res) => {
+	Game.findByIdAndUpdate(req.params.id, req.body, () => {
+		res.redirect('/games')	
+	});
+});
 //destroy
-
+router.delete('/:id', (req, res) => {
+	Game.findOneAndDelete(req.params.id, () => {
+		res.redirect('/games')
+	});
+});
 
 
 
--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const app = express();
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const PORT = 3000;
+//db
+require('./db/db')
+//controller
 const gameController = require('./controllers/gameController')
 
-require('./db/db')
-
+//middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'))
-
 app.use('/games', gameController)
+
 
 app.get('/', (req, res) => {
 	res.render('index.ejs')
